0e3d393da2b0aa6127d0ef5a873f2f40260dd8f1
luoyijiucheng <1784525940@qq.com>
2019-12-16 00:40:54 +0800

    完成文件上传

---
+++ src/main/java/com/yeqifu/sys/common/AppFileUtils.java
@@ -0,0 +1,78 @@
+package com.yeqifu.sys.common;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2019/12/15 23:44
+ */
+public class AppFileUtils {
+
+    //文件上传的保存路径
+    /**
+     * 默认值
+     */
+    public static String UPLOAD_PATH="E:/upload/";
+
+    static {
+        //读取配置文件的存储地址
+        InputStream stream = AppFileUtils.class.getClassLoader().getResourceAsStream("file.properties");
+        Properties properties=new Properties();
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String property = properties.getProperty("filepath");
+        if(null!=property) {
+            UPLOAD_PATH=property;
+        }
+    }
+
+    /**
+     * 根据文件老名字得到新名字
+     * @param oldName
+     * @return
+     */
+    public static String createNewFileName(String oldName) {
+        String stuff=oldName.substring(oldName.lastIndexOf("."), oldName.length());
+        return IdUtil.simpleUUID().toUpperCase()+stuff;
+    }
+
+    /**
+     * 文件下载
+     * @param path
+     * @return
+     */
+    public static ResponseEntity<Object> createResponseEntity(String path) {
+        //1,构造文件对象
+        File file=new File(UPLOAD_PATH, path);
+        if(file.exists()) {
+            //将下载的文件，封装byte[]
+            byte[] bytes=null;
+            try {
+                bytes = FileUtil.readBytes(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //创建封装响应头信息的对象
+            HttpHeaders header=new HttpHeaders();
+            //封装响应内容类型(APPLICATION_OCTET_STREAM 响应的内容不限定)
+            header.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            //创建ResponseEntity对象
+            ResponseEntity<Object> entity= new ResponseEntity<Object>(bytes, header, HttpStatus.CREATED);
+            return entity;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/yeqifu/sys/controller/FileController.java
@@ -0,0 +1,64 @@
+package com.yeqifu.sys.controller;
+
+import cn.hutool.core.date.DateUtil;
+import com.yeqifu.sys.common.AppFileUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2019/12/15 23:46
+ */
+@RestController
+@RequestMapping("file")
+public class FileController {
+
+    /**
+     * 文件上传
+     */
+    @RequestMapping("uploadFile")
+    public Map<String,Object> uploadFile(MultipartFile mf) {
+        //1.得到文件名
+        String oldName = mf.getOriginalFilename();
+        //2.根据文件名生成新的文件名
+        String newName=AppFileUtils.createNewFileName(oldName);
+        //3.得到当前日期的字符串
+        String dirName= DateUtil.format(new Date(), "yyyy-MM-dd");
+        //4.构造文件夹
+        File dirFile=new File(AppFileUtils.UPLOAD_PATH,dirName);
+        //5.判断当前文件夹是否存在
+        if(!dirFile.exists()) {
+            //创建文件夹
+            dirFile.mkdirs();
+        }
+        //6.构造文件对象
+        File file=new File(dirFile, newName);
+        //7,把mf里面的图片信息写入file
+        try {
+            mf.transferTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("path",dirFile+"/"+newName);
+        return map;
+    }
+
+
+    /**
+     * 图片下载
+     */
+    @RequestMapping("showImageByPath")
+    public ResponseEntity<Object> showImageByPath(String path){
+        return AppFileUtils.createResponseEntity(path);
+    }
+
+}
